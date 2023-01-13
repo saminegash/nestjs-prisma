911fee7af97a36d910560581f7c8a6549414dfb8
Sami Negash <smlnegash@gmail.com>
2023-01-13 07:01:33 +0300

    prisma setup

---
+++ prisma/migrations/20230113035916_addbooks/migration.sql
@@ -0,0 +1,16 @@
+/*
+  Warnings:
+
+  - You are about to drop the `Book` table. If the table is not empty, all the data it contains will be lost.
+
+*/
+-- DropTable
+PRAGMA foreign_keys=off;
+DROP TABLE "Book";
+PRAGMA foreign_keys=on;
+
+-- CreateTable
+CREATE TABLE "books" (
+    "id" INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT,
+    "title" TEXT NOT NULL
+);
